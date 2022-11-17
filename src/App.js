162a45931aee81a6800cddbee640b79d92c08017
as4lar <114438805+as4lar@users.noreply.github.com>
2022-11-17 11:53:11 -0600

    Update App.js
    Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import "./styles/App.css";
 import Pokemons from "./components/Pokemons";
 
+import React, {useState} from 'react';
+import Pokemons from "./components/Pokemons";
+//sesion por default no iniciada hasta que se presione el boton
 function App() {
-
-    return (
+  const [sesion, cambiarSesion]=useState(false); 
+  return (
+    <>
+      {sesion==true?(
+        <div className="App">
+        <header className="App-header">
+            <br/>
+            <h1 className="text-info">Pokemon</h1>
+            <br/>
+            <div className="container text-center">
+                <Pokemons/>
+            </div>
+        </header>
+    </div>
+      ):(
         <>
             <div className="App">
-                <header className="App-header">
-                    <br/>
-                    <h1 className="text-info">Rick and Morty</h1>
-                    <br/>
-                    <div className="container text-center">
-                        <Pokemons/>
-                    </div>
-                </header>
-            </div>
-
+                    <header className="App-header">
+                        <br/>
+                        <h1 className="text-info">Pokemon</h1>
+                        <br/>
+                        <div className="container text-center">
+                            <button onClick={()=> cambiarSesion(true)}>Siguiente</button>
+                        </div>
+                    </header>
+                </div>
         </>
-    );
+      )}
+
+
+    </>
+  );
 }
 
 export default App;
